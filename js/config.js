0f794738dfc34116e331175061eba4fd2b47d88b
blurry17 <ehuamanyauri@pucp.edu.pe>
2020-04-24 15:51:54 +0000

    Change config file for Cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_sM5UhA5Sf',
+        userPoolClientId: '7bhe0lpbadlnd18a919dg4u94n',
+        region: 'us-east-2'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
